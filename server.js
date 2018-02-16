48bfff09e31a2f420d860b8ef86221d652ae4261
adityasbg <35772932+adityasbg@users.noreply.github.com>
2018-02-16 20:50:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+
+
 app.get('/article-one', function (req , res){
   res.sendFile(path.join(__dirname, 'ui','article-one.html'));
 });
