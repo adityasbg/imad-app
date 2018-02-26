c269927b5d771c5042b949424d2ead69ae85ffd1
adityasbg <35772932+adityasbg@users.noreply.github.com>
2018-02-26 23:02:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,11 @@ function createTemplate (data){
     }
     
     
-    
+    app.get('/:articleName', function (req , res){
+var articleName=   req.params.articleName;
+
+  res.send(createTemplate(articles[articleName]));
+});
     
     
 
