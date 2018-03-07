54c6637b3a647eab2c34b9ba69ed216276e63a49
adityasbg <35772932+adityasbg@users.noreply.github.com>
2018-03-07 22:58:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,41 @@
-console.log('Loaded!');
+
+
+
+
+var submit = document.getElementById('submit_btn');
+
+
+submit.onclick = function () {
+    // Make a request to the server and send the name ;
+    var request = new XMLHttpRequest();
+
+    // capture a list of name and render it as a list 
+
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE) {
+
+            if (request.status === 200) {
+                console.log('user is loged in'); 
+            }
+            else if(reqest.status === 403){
+                alert('username / password incorect')
+            }
+            else if( request.status === 500) {
+                alert('something went wrong with the server '); 
+                 
+            }
+        };
+
+
+
+
+    };
+    var username = document.getElementById('username').value;
+      var password = document.getElementById('password').value;
+ console.log(username);
+ console.log(password);
+    request.open('POST', "http://adityaghoshsbg.imad.hasura-app.io/login", true);
+    request.setRequestHeader('Content Type ', 'application/json')
+    request.send(JSON.stringify({username:username,password:password}));
+
+};
