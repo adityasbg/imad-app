67e6626d6007df4c640a74745d26f3107da4e1be
adityasbg <35772932+adityasbg@users.noreply.github.com>
2018-03-07 22:58:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var crypto = require('crypto');
 
 var Pool =require('pg').Pool;
+var bodyParser = require('body-parser');
 
 var config ={
     user:'adityaghoshsbg',
@@ -15,6 +16,7 @@ var config ={
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());  
 
 
 
@@ -23,7 +25,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 function hash(input,salt){
-      var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+      var hashed = crypto.pbkdf2Sync(input ,salt,10000,512,'sha512');
       return ['pbkdf2', "10000",hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input',function(req,res){
@@ -32,7 +34,25 @@ app.get('/hash/:input',function(req,res){
 });
     
     
-
+app.post ('/create-user', function(req, res){
+    // username , password 
+    
+    var  username = req.body.username;
+    var password = req.body.password; 
+    var salt = crypto.RandomBytes(128).toString('hex');
+    var dbString =hash(password ,salt );
+    pool.query('INSERT INTO "user" (username , password) VALUES ($1 , $2) ',[username , dbString], function(err , result ){
+        if(err){
+            res.status(500).send(err.toString());
+            
+        }
+        else{
+            res.send('User successfully created '+username )  ;
+        }
+        
+    });
+    
+});
 
 var pool = new Pool(config);
 
