f9756ebcf9a19283c81c31521750069188f912ec
adityasbg <35772932+adityasbg@users.noreply.github.com>
2018-03-07 23:04:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ submit.onclick = function () {
       var password = document.getElementById('password').value;
  console.log(username);
  console.log(password);
-    request.open('POST', "http://adityaghoshsbg.imad.hasura-app.io/login", true);
+    request.open('POST', "http://adityaghoshsbg.imad.hasura-app.io", true);
     request.setRequestHeader('Content Type ', 'application/json')
     request.send(JSON.stringify({username:username,password:password}));
 
