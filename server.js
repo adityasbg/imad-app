1f87abdf1aea781df3675c5c4709a20638896280
adityasbg <35772932+adityasbg@users.noreply.github.com>
2018-03-08 18:51:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post ('/create-user', function(req, res){
             
         }
         else{
-            res.send('User successfully created '+username )  ;
+            res.send('User successfully created '+username );
         }
         
     });
